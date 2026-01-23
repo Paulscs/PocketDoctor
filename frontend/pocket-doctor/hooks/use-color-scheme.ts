4684c1ce11b1d6565008274601868e90ba58e80e
Jose Arturo Guzman <125223327+JoseArturoGuzman@users.noreply.github.com>
2026-01-22 22:34:02 -0400

    Force claro

--- frontend/pocket-doctor/hooks/use-color-scheme.ts
+++ frontend/pocket-doctor/hooks/use-color-scheme.ts
@@ -1,1 +1,6 @@
-export { useColorScheme } from "react-native";
+/**
+ * Force the app to always use light mode, ignoring system settings.
+ */
+export function useColorScheme() {
+  return 'light';
+}
